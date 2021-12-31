cee10aaedfc67f12465cc2484915cdfd32463b00
maxthunder <maxwell.stark@gmail.com>
2021-12-30 23:10:04 -0700

    finish up basic app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';// 'React' is redundant: already auto-imported
-import { Transition } from 'react-transition-group';
 
 // components
 import Form from './components/Form'
 import TodoList from './components/TodoList'
 import ApiService from './services/ApiService';
 
-  const defaultStyle = {
-    transition: `transform 200ms, opacity 200ms ease`,
-    opacity: 1
-  };
-
-  const transitionStyles = {
-    entering: { transform: 'scale(0.5)', opacity: 0 }, 
-    entered: { transform: 'scale(2.0)', opacity: 1},
-    exiting: { opacity: 0 },
-    exited: { opacity: 0 }
-  };
-  
-
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
-  const [entered, setEntered] = useState(false);
 
   const filterHandler = () => {
     switch(status) {
@@ -53,7 +38,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Max Thunder's Todo-List {inputText}</h1>
+        <h1>Max Thunder's Todo-List</h1>
       </header>
       <Form 
         inputText={inputText} 
@@ -63,20 +48,8 @@ function App() {
         setStatus={setStatus}
       />
       <TodoList filteredTodos={filteredTodos} todos={todos} setTodos={setTodos} />
-      <AComponent in={entered} />
-      <button onClick={() => {setEntered(!entered)}}>Toggle Entered</button>
     </div>
   );
 }
-
-const AComponent = ({ in: inProp }) => (
-  <Transition in={inProp} timeout={{appear: 100, enter: 300, exit: 300}} appear unmountOnExit>
-    {state => (
-      <div style={{...defaultStyle, ...transitionStyles[state]}}>
-        I am {state}
-      </div>
-    )}
-  </Transition>
-); 
 
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,28 +4,15 @@ import Todo from './Todo'
 // const TASK_PATH = 'https://todo-maxthunder-svc.herokuapp.com/tasks';
 
 const TodoList = ({ todos, setTodos, filteredTodos }) => {
-    const uncompletedTodos = todos.filter((todo) => !todo.isCompleted)
-    const completedTodos = todos.filter((todo) => todo.isCompleted)
+    // const uncompletedTodos = todos.filter((todo) => !todo.isCompleted)
+    // const completedTodos = todos.filter((todo) => todo.isCompleted)
 
     return (
         <div className="todo-container">
             <ul className="todo-list">
-                {uncompletedTodos.map((todo) => (
+                {filteredTodos.map((todo) => (
                     <Todo
-                        todos={todos}
-                        setTodos={setTodos}
-                        text={todo.description}
-                        key={Math.random().toString(36).substr(2, 9)} // 'key' is required when using map() to ensure uniqueness
-                        todo={todo}
-                    />
-                ))}
-            </ul>
-            <br/>
-            <div>- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - </div>
-            <ul className="todo-list">
-                {completedTodos.map((todo) => (
-                    <Todo
-                        todos={todos}
+                        todos={filteredTodos}
                         setTodos={setTodos}
                         text={todo.description}
                         key={Math.random().toString(36).substr(2, 9)} // 'key' is required when using map() to ensure uniqueness
@@ -34,6 +21,32 @@ const TodoList = ({ todos, setTodos, filteredTodos }) => {
                 ))}
             </ul>
         </div>
+        // <div className="todo-container">
+        //     <ul className="todo-list">
+        //         {uncompletedTodos.map((todo) => (
+        //             <Todo
+        //                 todos={todos}
+        //                 setTodos={setTodos}
+        //                 text={todo.description}
+        //                 key={Math.random().toString(36).substr(2, 9)} // 'key' is required when using map() to ensure uniqueness
+        //                 todo={todo}
+        //             />
+        //         ))}
+        //     </ul>
+        //     <br/>
+        //     <div>- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - </div>
+        //     <ul className="todo-list">
+        //         {completedTodos.map((todo) => (
+        //             <Todo
+        //                 todos={todos}
+        //                 setTodos={setTodos}
+        //                 text={todo.description}
+        //                 key={Math.random().toString(36).substr(2, 9)} // 'key' is required when using map() to ensure uniqueness
+        //                 todo={todo}
+        //             />
+        //         ))}
+        //     </ul>
+        // </div>
     )
 }
 
